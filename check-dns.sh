9b12efa0a4925d479936da7bdca686cf72ac4aa3
arthcp <arth.c.patel@gmail.com>
2023-10-28 14:28:10 +0530

    feat: retry mech to avoid random errors

--- check-dns.sh
+++ check-dns.sh
@@ -1,44 +1,57 @@
 #!/bin/bash
 
-cd /tmp
-
-# Check if old dns file exists
-if [ -e /tmp/dnsold_$1 ]
-then
-    # Get dns resolution and store in new file
-    host $1 | grep address | awk '{print $NF}' | sort | uniq | xargs echo > /tmp/dnsnew_$1
-
-    # Check if dns resolution worked
-    if [ $? -ne 0 ]
-    then
-        curl -H "Content-Type: application/json" -d "{\"username\": \"$MACHINE_NAME\", \"content\": \"DNS resolution failed for $1\"}" $DISCORD_WEBHOOK
-
-    else
-        # Compare old and new dns files
-        cmp -s /tmp/dnsold_$1 /tmp/dnsnew_$1
-
-        # If dns resolution changed, send notification to discord
-        if [ $? -ne 0 ]
-        then
-            dnsold=$(<dnsold_$1)
-            dnsnew=$(<dnsnew_$1)
-            curl -H "Content-Type: application/json" -d "{\"username\": \"$MACHINE_NAME\", \"content\": \"DNS resolution changed for $1 \n old: \`$dnsold\` \n new: \`$dnsnew\`\"}" $DISCORD_WEBHOOK
-        fi
-
-        # Copy new dns file to old dns file
-        cp /tmp/dnsnew_$1 /tmp/dnsold_$1
-    fi
-
-else
-    # Get dns resolution and store in old file
-    host $1 | grep address | awk '{print $NF}' | sort | uniq | xargs echo > /tmp/dnsold_$1
-
-    # Check if dns resolution worked
-    if [ $? -ne 0 ]
-    then
-        curl -H "Content-Type: application/json" -d "{\"username\": \"$MACHINE_NAME\", \"content\": \"DNS resolution failed for $1\"}" $DISCORD_WEBHOOK
-
-    else
-        curl -H "Content-Type: application/json" -d "{\"username\": \"$MACHINE_NAME\", \"content\": \"DNS resolution check started for $1\"}" $DISCORD_WEBHOOK
-    fi
+# Check for required input
+if [ -z "$1" ]; then
+    echo "Domain not provided!"
+    exit 1
 fi
+
+TMP_DIR="/tmp"
+DNS_OLD="${TMP_DIR}/dnsold_$1"
+DNS_NEW="${TMP_DIR}/dnsnew_$1"
+
+# Function to send Discord alert
+send_alert() {
+    curl -H "Content-Type: application/json" -d "{\"username\": \"$MACHINE_NAME\", \"content\": \"$1\"}" $DISCORD_WEBHOOK
+}
+
+retry=5
+
+while [ $retry -gt 0 ]; do
+    # Retrieve DNS resolution and store in new file
+    host $1 | grep address | awk '{print $NF}' | sort | uniq | xargs echo > $DNS_NEW
+
+    if [ $? -ne 0 ]; then
+        ((retry--))
+        if [ $retry -eq 0 ]; then
+            send_alert "DNS resolution failed for $1 after multiple attempts"
+            exit 1
+        fi
+        sleep 5
+        continue
+    fi
+
+    # If old DNS doesn't exist, this is the first run
+    if [ ! -e $DNS_OLD ]; then
+        cp $DNS_NEW $DNS_OLD
+        send_alert "DNS resolution check started for $1"
+        exit 0
+    fi
+
+    # Compare old and new DNS resolutions
+    if ! cmp -s $DNS_OLD $DNS_NEW; then
+        ((retry--))
+        if [ $retry -eq 0 ]; then
+            dnsold=$(<$DNS_OLD)
+            dnsnew=$(<$DNS_NEW)
+            send_alert "DNS resolution changed for $1 \n old: \`$dnsold\` \n new: \`$dnsnew\` after multiple checks"
+            cp $DNS_NEW $DNS_OLD
+            exit 0
+        fi
+        sleep 5
+        continue
+    fi
+
+    # If no problems, break out of loop
+    break
+done
--- check-website.sh
+++ check-website.sh
@@ -1,50 +1,70 @@
 #!/bin/bash
 
-cd /tmp
-
-# Check if old hash file exists
-if [ -e /tmp/hashold_$1 ]
-then
-    # download website
-    wget -r -k -l 7 -p -E -nc -nv https://$1/ 1>&2 2>/dev/null || true
-
-    # Get website hash and store in new file
-    find $1 -type f | grep -v robots.txt | xargs md5sum | md5sum | cut -d\  -f1 > /tmp/hashnew_$1
-
-    # Check if website hash generation worked
-    if [ $? -ne 0 ]
-    then
-        curl -H "Content-Type: application/json" -d "{\"username\": \"$MACHINE_NAME\", \"content\": \"Website content hash failed for $1\"}" $DISCORD_WEBHOOK
-
-    else
-        # Compare old and new website hash files
-        cmp -s /tmp/hashold_$1 /tmp/hashnew_$1
-
-        # If website hash changed, send notification to discord
-        if [ $? -ne 0 ]
-        then
-            hashold=$(<hashold_$1)
-            hashnew=$(<hashnew_$1)
-            curl -H "Content-Type: application/json" -d "{\"username\": \"$MACHINE_NAME\", \"content\": \"Website content hash changed for $1 \n old: \`$hashold\` \n new: \`$hashnew\`\"}" $DISCORD_WEBHOOK
-        fi
-
-        # Copy new website hash file to old website hash file
-        cp /tmp/hashnew_$1 /tmp/hashold_$1
-    fi
-
-else
-    # download website
-    wget -r -k -l 7 -p -E -nc -nv https://$1/ 1>&2 2>/dev/null || true
-
-    # Get website hash and store in old file
-    find $1 -type f | grep -v robots.txt | xargs md5sum | md5sum | cut -d\  -f1 > /tmp/hashold_$1
-
-    # Check if website hash generation worked
-    if [ $? -ne 0 ]
-    then
-        curl -H "Content-Type: application/json" -d "{\"username\": \"$MACHINE_NAME\", \"content\": \"Website content hash failed for $1\"}" $DISCORD_WEBHOOK
-
-    else
-        curl -H "Content-Type: application/json" -d "{\"username\": \"$MACHINE_NAME\", \"content\": \"Website content hash check started for $1\"}" $DISCORD_WEBHOOK
-    fi
+# Check for required input
+if [ -z "$1" ]; then
+    echo "Domain not provided!"
+    exit 1
 fi
+
+TMP_DIR="/tmp"
+HASH_OLD="${TMP_DIR}/hashold_$1"
+HASH_NEW="${TMP_DIR}/hashnew_$1"
+
+# Function to send Discord alert
+send_alert() {
+    curl -H "Content-Type: application/json" -d "{\"username\": \"$MACHINE_NAME\", \"content\": \"$1\"}" $DISCORD_WEBHOOK
+}
+
+retry=5
+
+while [ $retry -gt 0 ]; do
+    # Download the website
+    wget -r -k -l 7 -p -E -nc -nv --timeout=5 https://$1/ -P $TMP_DIR 1>&2 2>/dev/null
+
+    if [ $? -ne 0 ]; then
+        ((retry--))
+        if [ $retry -eq 0 ]; then
+            send_alert "Failed to download website for $1 after multiple attempts"
+            exit 1
+        fi
+        sleep 5
+        continue
+    fi
+
+    # Generate website hash
+    find ${TMP_DIR}/$1 -type f | grep -v robots.txt | xargs md5sum | md5sum | cut -d\  -f1 > $HASH_NEW
+
+    if [ $? -ne 0 ]; then
+        ((retry--))
+        if [ $retry -eq 0 ]; then
+            send_alert "Website content hash generation failed for $1 after multiple attempts"
+            exit 1
+        fi
+        sleep 5
+        continue
+    fi
+
+    # If old hash doesn't exist, this is the first run
+    if [ ! -e $HASH_OLD ]; then
+        cp $HASH_NEW $HASH_OLD
+        send_alert "Website content hash check started for $1"
+        exit 0
+    fi
+
+    # Compare old and new hashes
+    if ! cmp -s $HASH_OLD $HASH_NEW; then
+        ((retry--))
+        if [ $retry -eq 0 ]; then
+            hashold=$(<$HASH_OLD)
+            hashnew=$(<$HASH_NEW)
+            send_alert "Website content hash changed for $1 \n old: \`$hashold\` \n new: \`$hashnew\` after multiple checks"
+            cp $HASH_NEW $HASH_OLD
+            exit 0
+        fi
+        sleep 5
+        continue
+    fi
+
+    # If no problems, break out of loop
+    break
+done
